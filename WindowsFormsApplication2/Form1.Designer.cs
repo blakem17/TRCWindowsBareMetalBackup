ba3f7ec231a143d3ce9d80ab8fba55473721434c
user <user@PC1>
2016-07-16 20:27:28 -0700

    Add project files.

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.directoryBox = new System.Windows.Forms.TextBox();
+            this.directoryLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.companyTB = new System.Windows.Forms.TextBox();
+            this.credTB = new System.Windows.Forms.TextBox();
+            this.directoryTB = new System.Windows.Forms.TextBox();
+            this.emailToTB = new System.Windows.Forms.TextBox();
+            this.emailFRTB = new System.Windows.Forms.TextBox();
+            this.logTB = new System.Windows.Forms.TextBox();
+            this.smtpTB = new System.Windows.Forms.TextBox();
+            this.companyLB = new System.Windows.Forms.Label();
+            this.credLB = new System.Windows.Forms.Label();
+            this.directoryLB = new System.Windows.Forms.Label();
+            this.emailToLB = new System.Windows.Forms.Label();
+            this.emailFRLB = new System.Windows.Forms.Label();
+            this.logLB = new System.Windows.Forms.Label();
+            this.pathTyLB = new System.Windows.Forms.Label();
+            this.smtpLB = new System.Windows.Forms.Label();
+            this.pathTYCB = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(311, 41);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 219);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(687, 307);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // directoryBox
+            // 
+            this.directoryBox.Location = new System.Drawing.Point(26, 29);
+            this.directoryBox.Name = "directoryBox";
+            this.directoryBox.Size = new System.Drawing.Size(279, 20);
+            this.directoryBox.TabIndex = 3;
+            // 
+            // directoryLabel
+            // 
+            this.directoryLabel.AutoSize = true;
+            this.directoryLabel.Location = new System.Drawing.Point(26, 10);
+            this.directoryLabel.Name = "directoryLabel";
+            this.directoryLabel.Size = new System.Drawing.Size(49, 13);
+            this.directoryLabel.TabIndex = 4;
+            this.directoryLabel.Text = "Directory";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // companyTB
+            // 
+            this.companyTB.Location = new System.Drawing.Point(29, 91);
+            this.companyTB.Name = "companyTB";
+            this.companyTB.Size = new System.Drawing.Size(161, 20);
+            this.companyTB.TabIndex = 5;
+            // 
+            // credTB
+            // 
+            this.credTB.Location = new System.Drawing.Point(29, 117);
+            this.credTB.Name = "credTB";
+            this.credTB.Size = new System.Drawing.Size(161, 20);
+            this.credTB.TabIndex = 6;
+            // 
+            // directoryTB
+            // 
+            this.directoryTB.Location = new System.Drawing.Point(29, 143);
+            this.directoryTB.Name = "directoryTB";
+            this.directoryTB.Size = new System.Drawing.Size(161, 20);
+            this.directoryTB.TabIndex = 7;
+            // 
+            // emailToTB
+            // 
+            this.emailToTB.Location = new System.Drawing.Point(29, 169);
+            this.emailToTB.Name = "emailToTB";
+            this.emailToTB.Size = new System.Drawing.Size(161, 20);
+            this.emailToTB.TabIndex = 8;
+            // 
+            // emailFRTB
+            // 
+            this.emailFRTB.Location = new System.Drawing.Point(363, 91);
+            this.emailFRTB.Name = "emailFRTB";
+            this.emailFRTB.Size = new System.Drawing.Size(161, 20);
+            this.emailFRTB.TabIndex = 10;
+            // 
+            // logTB
+            // 
+            this.logTB.Location = new System.Drawing.Point(363, 117);
+            this.logTB.Name = "logTB";
+            this.logTB.Size = new System.Drawing.Size(161, 20);
+            this.logTB.TabIndex = 11;
+            // 
+            // smtpTB
+            // 
+            this.smtpTB.Location = new System.Drawing.Point(363, 169);
+            this.smtpTB.Name = "smtpTB";
+            this.smtpTB.Size = new System.Drawing.Size(161, 20);
+            this.smtpTB.TabIndex = 12;
+            // 
+            // companyLB
+            // 
+            this.companyLB.AutoSize = true;
+            this.companyLB.Location = new System.Drawing.Point(207, 91);
+            this.companyLB.Name = "companyLB";
+            this.companyLB.Size = new System.Drawing.Size(51, 13);
+            this.companyLB.TabIndex = 13;
+            this.companyLB.Text = "Company";
+            // 
+            // credLB
+            // 
+            this.credLB.AutoSize = true;
+            this.credLB.Location = new System.Drawing.Point(207, 120);
+            this.credLB.Name = "credLB";
+            this.credLB.Size = new System.Drawing.Size(59, 13);
+            this.credLB.TabIndex = 14;
+            this.credLB.Text = "Credentials";
+            // 
+            // directoryLB
+            // 
+            this.directoryLB.AutoSize = true;
+            this.directoryLB.Location = new System.Drawing.Point(207, 146);
+            this.directoryLB.Name = "directoryLB";
+            this.directoryLB.Size = new System.Drawing.Size(49, 13);
+            this.directoryLB.TabIndex = 15;
+            this.directoryLB.Text = "Directory";
+            // 
+            // emailToLB
+            // 
+            this.emailToLB.AutoSize = true;
+            this.emailToLB.Location = new System.Drawing.Point(207, 172);
+            this.emailToLB.Name = "emailToLB";
+            this.emailToLB.Size = new System.Drawing.Size(76, 13);
+            this.emailToLB.TabIndex = 16;
+            this.emailToLB.Text = "Send Email To";
+            // 
+            // emailFRLB
+            // 
+            this.emailFRLB.AutoSize = true;
+            this.emailFRLB.Location = new System.Drawing.Point(553, 94);
+            this.emailFRLB.Name = "emailFRLB";
+            this.emailFRLB.Size = new System.Drawing.Size(54, 13);
+            this.emailFRLB.TabIndex = 17;
+            this.emailFRLB.Text = "email from";
+            // 
+            // logLB
+            // 
+            this.logLB.AutoSize = true;
+            this.logLB.Location = new System.Drawing.Point(553, 117);
+            this.logLB.Name = "logLB";
+            this.logLB.Size = new System.Drawing.Size(25, 13);
+            this.logLB.TabIndex = 18;
+            this.logLB.Text = "Log";
+            // 
+            // pathTyLB
+            // 
+            this.pathTyLB.AutoSize = true;
+            this.pathTyLB.Location = new System.Drawing.Point(553, 150);
+            this.pathTyLB.Name = "pathTyLB";
+            this.pathTyLB.Size = new System.Drawing.Size(56, 13);
+            this.pathTyLB.TabIndex = 19;
+            this.pathTyLB.Text = "Path Type";
+            // 
+            // smtpLB
+            // 
+            this.smtpLB.AutoSize = true;
+            this.smtpLB.Location = new System.Drawing.Point(553, 176);
+            this.smtpLB.Name = "smtpLB";
+            this.smtpLB.Size = new System.Drawing.Size(61, 13);
+            this.smtpLB.TabIndex = 20;
+            this.smtpLB.Text = "smtp server";
+            // 
+            // pathTYCB
+            // 
+            this.pathTYCB.FormattingEnabled = true;
+            this.pathTYCB.Location = new System.Drawing.Point(363, 143);
+            this.pathTYCB.Name = "pathTYCB";
+            this.pathTYCB.Size = new System.Drawing.Size(161, 21);
+            this.pathTYCB.TabIndex = 21;
+            this.pathTYCB.SelectedIndexChanged += new System.EventHandler(this.pathTYCB_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 538);
+            this.Controls.Add(this.pathTYCB);
+            this.Controls.Add(this.smtpLB);
+            this.Controls.Add(this.pathTyLB);
+            this.Controls.Add(this.logLB);
+            this.Controls.Add(this.emailFRLB);
+            this.Controls.Add(this.emailToLB);
+            this.Controls.Add(this.directoryLB);
+            this.Controls.Add(this.credLB);
+            this.Controls.Add(this.companyLB);
+            this.Controls.Add(this.smtpTB);
+            this.Controls.Add(this.logTB);
+            this.Controls.Add(this.emailFRTB);
+            this.Controls.Add(this.emailToTB);
+            this.Controls.Add(this.directoryTB);
+            this.Controls.Add(this.credTB);
+            this.Controls.Add(this.companyTB);
+            this.Controls.Add(this.directoryLabel);
+            this.Controls.Add(this.directoryBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox directoryBox;
+        private System.Windows.Forms.Label directoryLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox companyTB;
+        private System.Windows.Forms.TextBox credTB;
+        private System.Windows.Forms.TextBox directoryTB;
+        private System.Windows.Forms.TextBox emailToTB;
+        private System.Windows.Forms.TextBox emailFRTB;
+        private System.Windows.Forms.TextBox logTB;
+        private System.Windows.Forms.TextBox smtpTB;
+        private System.Windows.Forms.Label companyLB;
+        private System.Windows.Forms.Label credLB;
+        private System.Windows.Forms.Label directoryLB;
+        private System.Windows.Forms.Label emailToLB;
+        private System.Windows.Forms.Label emailFRLB;
+        private System.Windows.Forms.Label logLB;
+        private System.Windows.Forms.Label pathTyLB;
+        private System.Windows.Forms.Label smtpLB;
+        private System.Windows.Forms.ComboBox pathTYCB;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private const string noSelStr = "Please select a directory";
+        private string filelocation = "";
+        private string compIdent = "$companyName =";
+        private string emToIdent = "$emailTo =";
+        private string emFrIdent = "$emailFrom =";
+        private string smtpIdent = "$smtpServer =";
+        private string pthTpIdent = "$pathType =";
+        private string credIdent = "$credential =";
+        private string dirIdent = "$dir =";
+        private string lgFlIdent = "$logFile =";
+        private string[] setArr = new string[8];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            directoryBox.Text = "";
+            setArr[0] = compIdent;
+            setArr[1] = credIdent;
+            setArr[2] = dirIdent;
+            setArr[3] = emFrIdent;
+            setArr[4] = emToIdent;
+            setArr[5] = lgFlIdent;
+            setArr[6] = pthTpIdent;
+            setArr[7] = smtpIdent;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            textBox.Text = filelocation;
+            if  (directoryBox.Text.Equals(noSelStr))
+            {
+                textBox.Text = noSelStr;
+            }
+            if (directoryBox.Text.Equals(""))
+            {
+                textBox.Text = noSelStr;
+                directoryBox.Text = noSelStr;
+            }
+            if (File.Exists(filelocation))
+            {
+                textBox.Text = "";
+                string[] fileTextArr = File.ReadAllLines(filelocation);
+                string fileText = ConvertStringArrayToString(fileTextArr);
+                foreach (string s in fileTextArr)
+                {
+                    foreach (string t in setArr)
+                    {
+                    if (s.Contains(t))
+                    {
+                        textBox.AppendText(Environment.NewLine + s);
+                    }
+                        if (s.Contains(setArr[0]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"", "");
+                                companyTB.Text = u;
+                            }
+                        }
+                        if (s.Contains(setArr[1]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"","");
+                                credTB.Text = u;
+                            }
+                        }
+                        if (s.Contains(setArr[2]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"", "");
+                                directoryBox.Text = u;
+                            }
+                        }
+                        if (s.Contains(setArr[3]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"", "");
+                                emailToTB.Text = u;
+                            }
+                        }
+                        if (s.Contains(setArr[4]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"", "");
+                                emailFRTB.Text = u;
+                            }
+                        }
+                        if (s.Contains(setArr[5]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"", "");
+                                logTB.Text = u;
+                            }
+                        }
+                        if (s.Contains(setArr[6]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"", "");
+                                pathTYCB.Text = u;
+                            }
+                        }
+                        if (s.Contains(setArr[7]))
+                        {
+                            var reg = new Regex("\".*?\"");
+                            var matches = reg.Matches(s);
+                            foreach (var item in matches)
+                            {
+                                string u = item.ToString().Replace("\"","");
+                                smtpTB.Text = u;
+                            }
+
+                        }
+                    }
+            }
+
+            }
+            else
+            {
+                if(!File.Exists(filelocation) && directoryBox.Text.Equals(""))
+                {
+                    textBox.Text = "File Does Not Exit";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.ShowDialog();
+            filelocation = openFileDialog1.InitialDirectory + openFileDialog1.FileName;
+            if (filelocation.Equals("openFileDialog1"))
+            {
+                directoryBox.Text = noSelStr;
+            }
+            else
+            {
+                directoryBox.Text = filelocation;
+            }
+        }
+        static string ConvertStringArrayToString(string[] array)
+        {
+            //
+            // Concatenate all the elements into a StringBuilder.
+            //
+            StringBuilder builder = new StringBuilder();
+            foreach (string value in array)
+            {
+                builder.Append(value);
+                builder.Append('.');
+            }
+            return builder.ToString();
+        }
+
+        private void pathTYCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
